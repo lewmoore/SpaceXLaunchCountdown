e28a150490a623cc8b2594be6faf2ce45f2f8ca6
lewmoore <lewis.moore2707@gmail.com>
2018-09-18 14:45:50 +0100

    Change to es6 syntax

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 fetch('https://api.spacexdata.com/v2/launches/next')
-.then(function(response) {
+.then((response) => {
     return response.json();
 })
-.then(function(json) {
+.then((json) => {
     let nextLaunch = document.getElementById("demo").innerHTML = json.launch_date_local
-    
-    var deadline = new Date(nextLaunch).getTime();
-    var x = setInterval(function() {
-    var now = new Date().getTime();
-    var t = deadline - now;
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+    let deadline = new Date(nextLaunch).getTime();
+    let x = setInterval(function() {
+    let now = new Date().getTime();
+    let t = deadline - now;
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+    let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((t % (1000 * 60)) / 1000);
     document.getElementById("countdown").innerHTML = days + "d " 
     + hours + "h " + minutes + "m " + seconds + "s ";
         if (t < 0) {
