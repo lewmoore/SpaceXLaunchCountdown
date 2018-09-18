867506130ab8ec2ccb5f36237653fd4034a6f89b
lewmoore <lewis.moore2707@gmail.com>
2018-09-18 12:03:25 +0100

    Initialise repo

---
+++ index.js
@@ -0,0 +1,26 @@
+fetch('https://api.spacexdata.com/v2/launches/next')
+.then(function(response) {
+    return response.json();
+})
+.then(function(json) {
+    let nextLaunch = document.getElementById("demo").innerHTML = json.launch_date_local
+    
+    var deadline = new Date(nextLaunch).getTime();
+    var x = setInterval(function() {
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerHTML = days + "d " 
+    + hours + "h " + minutes + "m " + seconds + "s ";
+        if (t < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+});
+
+
+
